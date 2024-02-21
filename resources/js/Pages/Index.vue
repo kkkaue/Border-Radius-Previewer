dd50b8b9caf31c98e23f48ad88267f7487d4ca99
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-02-21 13:02:12 -0300

    Configuração inicial

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <h1>
+        Welcome to the home page
+    </h1>
+    
+</template>
